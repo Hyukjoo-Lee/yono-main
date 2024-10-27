14dd4b2a71e9d3bd833a70479557d0c765aa3c6b
Hyukjoo-Lee <hjjaydon.lee@gmail.com>
2024-10-27 23:27:01 +0900

    Add: 마이카드 페이지, CommonTab 생성
    Update: CardAnaysis 에 생성되어 있는 공통된 디자인 export 처리

--- app-frontend/src/pages/mycard/MyCard.js
+++ app-frontend/src/pages/mycard/MyCard.js
@@ -1,7 +1,33 @@
+import React, { useState } from "react";
+import { Root, TextStyle, TitleStyle } from "../cardAnalysis/CardAnalysis";
+import CommonTabs from "../../common/CommonTabs";
+import CardRegTab from "./CardRegTab";
+import CardRecTab from "./CardRecTab";
+
 export function MyCard() {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  /**
+   * 띄울 탭 페이지
+   */
+  const panels = [
+    <CardRegTab />,
+    <CardRecTab />
+  ];
+
   return (
-    <div>
-      <h1>마이카드</h1>
-    </div>
+    <Root>
+      <CommonTabs
+        labels={["카드 등록", "카드 추천"]}
+        value={selectedTab}
+        onTabChange={setSelectedTab}
+      />
+      <TitleStyle>나의 카드 등록</TitleStyle>
+      <TextStyle>
+        소비패턴을 확인하고 싶은 카드로 등록하세요. <br />
+        등록하고 싶은 카드를 입력 후 카드 리스트에서 확인하세요.
+      </TextStyle>
+      {panels[selectedTab]}
+    </Root>
   );
 }
