46d0fe574363d694d1504f4a68add5134700cdfa
Hyukjoo-Lee <hjjaydon.lee@gmail.com>
2024-11-07 17:23:23 +0900

    Fix: babel-preset-react-app 워닝 해결 및 등가연산자 === 로 변경

--- app-frontend/src/pages/cardAnalysis/CardAnalysis.js
+++ app-frontend/src/pages/cardAnalysis/CardAnalysis.js
@@ -26,19 +26,19 @@ const CardAnalysis = () => {
       />
       <CommonPageInfo
         title={
-          selectedTab == 0
+          selectedTab === 0
             ? "일별통계(캘린더)"
-            : selectedTab == 1
+            : selectedTab === 1
             ? "월별통계(막대그래프)"
             : "목차별통계(원형그래프)"
         }
         text={
-          selectedTab == 0 ? (
+          selectedTab === 0 ? (
             <p>
               내가 선택한 날짜의 소비내역을 알려드립니다. <br />
               캘린더의 날짜 클릭하시면 소비 내역을 확인하실수 있습니다.
             </p>
-          ) : selectedTab == 1 ? (
+          ) : selectedTab === 1 ? (
             <p>
               유저가 선택한 최근 3개월의 소비내역을 알려드립니다. <br />
               현재 날짜 기준으로 막대,원형 그래프를 확인하실수 있습니다.
