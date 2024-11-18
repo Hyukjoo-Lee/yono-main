e44b6c5d0dc484c1abc36bfdfaab69d7d8a5ac0e
jihxn-kim <lizars4024@gmail.com>
2024-11-18 17:05:43 +0900

    Feat: 비밀번호 질문, 답변 추가 및 컴포넌트 정렬

--- app-frontend/src/pages/mypage/CheckUserInfo.js
+++ app-frontend/src/pages/mypage/CheckUserInfo.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import CustomButton from "../../common/CommonButton";
 import CommonInput from "../../common/CommonInput";
 import ImageGallery from "./ImageSelect";
@@ -10,122 +9,169 @@ import image2 from "../../assets/images/Character2.png";
 import image3 from "../../assets/images/Character3.png";
 import image4 from "../../assets/images/Character4.png";
 
-const images = [
-    image1,
-    image2,
-    image3,
-    image4,
-];
+const images = [image1, image2, image3, image4];
 
 const Root = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 15px;
+  width: 350px;
+  margin: 0 auto;
 `;
 
 const Root2 = styled.div`
-    width: 350px;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    margin-top: 10px;
-    gap: 5px;
+  width: 350px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  margin-top: 10px;
+  gap: 5px;
 `;
 
 const Button = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    margin-top: 30px;
-    gap: 15px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 30px;
+  gap: 15px;
+`;
+
+const InlineWrapper = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
+`;
+
+const HiddenSection = styled.div`
+  margin-top: 10px;
+  width: 350px;
+  display: ${({ visible }) => (visible ? "flex" : "none")};
+  flex-direction: column;
+  gap: 15px;
 `;
 
 const CheckUserInfo = ({
-    userId = "아이디",
-    password = "비밀번호",
-    nickname = "닉네임",
-    Target_Expenditure_Amout = "테스트 목표 지출금액",
+  userId = "아이디",
+  password = "비밀번호",
+  nickname = "닉네임",
+  Target_Expenditure_Amout = "테스트 목표 지출금액",
 }) => {
+  const [inputDisabled, setInputDisabled] = useState(true);
+  const [backgroundColor, setBackgroundColor] = useState("#F5F5F5");
+  const [showPasswordQuestion, setShowPasswordQuestion] = useState(false);
+  const [showPasswordEdited, setShowPasswordEdited] = useState(false);
 
-    const [inputDisabled, setInputDisabled] = useState(true);
-    const [backgroundColor, setbackgroundColor] = useState('#F5F5F5');
+  const modify = () => {
+    setInputDisabled(false);
+    setBackgroundColor("#F8F9FE");
+  };
 
-    const modify = () => {
-        setInputDisabled(false);
-        setbackgroundColor('#F8F9FE');
-    };
+  const togglePasswordQuestion = () => {
+    setShowPasswordQuestion(!showPasswordQuestion);
+  };
 
-    const commonInputProps = {
-        disabled: inputDisabled,
-        background: backgroundColor,
-        width: '350px',
-    };
+  const togglePasswordEdit= () => {
+    setShowPasswordEdited(!showPasswordEdited);
+  };
 
-    const commonButtonProps = {
-        width : '100px',
-        height : '38px',
-    };
+  const commonInputProps = {
+    disabled: inputDisabled,
+    background: backgroundColor,
+    width: "350px",
+  };
 
-    // 1. 비밀번호 수정시에 질문에 대한 답변 추가해야됨
-    // 2. 캐릭터 선택 input 아님
-    // 3. 
+  const commonButtonProps = {
+    width: "100px",
+    height: "38px",
+  };
 
-    return (
-        <>
-        <Root>
-            <CommonInput
-                placeholder = {userId}
-                text = "아이디"
-                disabled = "true"
-                background = "#F5F5F5"
-                width = '350px'
-            />
+  return (
+    <Root>
+      <CommonInput
+        placeholder={userId}
+        text="아이디"
+        disabled="true"
+        background="#F5F5F5"
+        width="350px"
+      />
 
-            <CommonInput
-                placeholder = {password}
-                text = "비밀번호"
-                {...commonInputProps}
-            />
+      <InlineWrapper>
+        <CommonInput
+          placeholder={password}
+          text="비밀번호"
+          {...commonInputProps}
+        />
+        <CustomButton
+          text="비밀번호 변경"
+          onClick={togglePasswordQuestion}
+          width="140px"
+          height="38px"
+        />
+      </InlineWrapper>
 
-            <CommonInput
-                placeholder = {nickname}
-                text = "닉네임"
-                {...commonInputProps}
-            />
+      <HiddenSection visible={showPasswordQuestion}>
+        <CommonInput
+          placeholder="비밀번호 질문"
+          disabled="true" 
+          text="질문"
+          width="350px"
+        />
 
-            <CommonInput
-                placeholder = {Target_Expenditure_Amout}
-                text = "이번 달 목표 지출금액"
-                {...commonInputProps}
-            />
+        <InlineWrapper>
+          <CommonInput
+            placeholder="비밀번호 답변"
+            text="답변"
+            width="350px"
+          />
+          <CustomButton
+            text="확인"
+            onClick={togglePasswordEdit}
+            width="140px"
+            height="38px"
+          />
+        </InlineWrapper>
+        <CommonInput
+          placeholder="수정할 비밀번호"
+          text="수정할 비밀번호를 입력하세요"
+          width="350px"
+        />
+      </HiddenSection>
 
-            <Root2>
-                캐릭터 선택
-                <ImageGallery images = {images}/>
-            </Root2>
-        </Root>
-        
-        
+      <CommonInput
+        placeholder={nickname}
+        text="닉네임"
+        {...commonInputProps}
+      />
 
-        <Button>
-            <CustomButton
-                text = '수정'
-                onClick = {modify}
-                {...commonButtonProps}
-            />
-            
-            <CustomButton 
-                text = '회원 탈퇴'
-                // onClick = {}
-                {...commonButtonProps}
-            />
+      <CommonInput
+        placeholder={Target_Expenditure_Amout}
+        text="이번 달 목표 지출금액"
+        {...commonInputProps}
+      />
 
-        </Button>
+      <Root2>
+        캐릭터 선택
+        <ImageGallery images={images} />
+      </Root2>
+      
+      <Button>
+        <CustomButton
+          text="저장"
+          onClick={modify}
+          {...commonButtonProps}
+        />
 
-        </>
-    );
+        <CustomButton 
+          text="회원 탈퇴"
+          // onClick={}
+          {...commonButtonProps}
+        />
+      </Button>
+    </Root>
+  );
 };
 
 export default CheckUserInfo;
