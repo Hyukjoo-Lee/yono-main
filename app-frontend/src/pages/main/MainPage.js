64abbfa7ff0aefdf38e9bd840d2269b8bf4db3e4
김채림 <rim1080@icloud.com>
2024-11-21 17:11:58 +0900

    add:main 폴더생성

---
+++ app-frontend/src/pages/main/MainPage.js
@@ -0,0 +1,25 @@
+
+// import { ReactComponent as HandsClapping } from "../assets/images/HandsClapping.svg";
+// import CustomButton from "../common/CommonButton";
+import React from "react";
+import styled from "styled-components";
+import CommonRoot from "../../common/CommonRoot";
+import MainBox from "./MainBox";
+import MainIntro from "./MainIntro";
+// import {SignUp} from "./auth/SignUp";
+
+const StyledWrap = styled.div`
+  width: 1200px;
+  height: 1000px;
+`;
+
+export function MainPage() {
+  return (
+    <CommonRoot>
+      <StyledWrap>
+        <MainIntro/>
+        <MainBox/>
+      </StyledWrap>
+    </CommonRoot>
+  );
+}
