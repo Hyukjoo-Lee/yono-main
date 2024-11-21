cbc5b97b5cb66be53a2455d4f7a9a8fed4b968af
LeeHyeonKyeong <lhk883@naver.com>
2024-11-21 21:18:22 +0900

    비밀번호 재설정

--- app-frontend/src/pages/auth/Component/ErrorMessage.js
+++ app-frontend/src/pages/auth/Component/ErrorMessage.js
@@ -5,11 +5,15 @@ export const EMPTY_PASSWORD1_ERROR = 'Please enter Password';
 export const EMPTY_PASSWORD2_ERROR = 'Please re-enter Password';
 export const EMPTY_CHECKBOX_ERROR =
   'Please check both Privacy Policy and Agreement';
-export const PASSWORD2_CONFORM_ERROR = 'Please confirm your Password';
+const PASSWORD2_CONFORM_ERROR = '비밀번호가 일치하지 않습니다';
 export const PASSWORD_REGEX_ERROR =
   'Password should be between 8 to 16 characters';
 export const EMAIL_REGEX_ERROR = 'Please enter a valid email address';
 const EMAIL_VERIFIED_ERROR = '아이디가 존재하지 않습니다.';
 const PASSWORD_VERIFIED_ERROR = '비밀번호가 틀립니다.';
 
-export { EMAIL_VERIFIED_ERROR, PASSWORD_VERIFIED_ERROR };
+export {
+  EMAIL_VERIFIED_ERROR,
+  PASSWORD_VERIFIED_ERROR,
+  PASSWORD2_CONFORM_ERROR,
+};
