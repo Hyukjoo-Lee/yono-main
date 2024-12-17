17b0f47e7b46ea3cf7529a254a63246d0358acf4
Hyukjoo Lee <jaydon@Hyukjoos-MacBook-Pro.local>
2024-12-18 01:50:23 +0900

    Add: api 통신을 위한 설정

---
+++ app-frontend/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    '/user',
+    createProxyMiddleware({
+      target: process.env.REACT_APP_API_BASE_URL,
+      changeOrigin: true,
+    }),
+  );
+};
