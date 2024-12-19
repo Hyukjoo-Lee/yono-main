bb72b3b3779c78cecd7587822f6b9bfe533e3c2e
Hyukjoo Lee <jaydon@Hyukjoos-MacBook-Pro.local>
2024-12-19 15:48:42 +0900

    Setting: dotenv 환경변수 파일 적용

--- app-backend/src/main/java/com/mmk/BackendApplication.java
+++ app-backend/src/main/java/com/mmk/BackendApplication.java
@@ -2,11 +2,17 @@ package com.mmk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
