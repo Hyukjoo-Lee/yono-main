f1995f1d32afee5068b3222f385fca0df381ca5f
김채림 <rim1080@icloud.com>
2024-12-30 17:44:19 +0900

    update : 소개페이지 on -> one 수정

--- app-frontend/src/pages/intro/Intro.js
+++ app-frontend/src/pages/intro/Intro.js
@@ -43,7 +43,7 @@ export function Intro() {
       img: Info_icon,
       title: 'YONO 소개',
       subtitle:
-        'YONO(You Need Only On)은 내가 사용하는 카드를 불러와 나의 소비패턴을 분석하고 절약이 필요한 부분을 알려줍니다.YONO의 주요 기능을 보시려면 해당하는 이모티콘을 클릭해보세요.',
+        'YONO(You Need Only One)은 내가 사용하는 카드를 불러와 나의 소비패턴을 분석하고 절약이 필요한 부분을 알려줍니다.YONO의 주요 기능을 보시려면 해당하는 이모티콘을 클릭해보세요.',
       content: Sample,
     },
     {
