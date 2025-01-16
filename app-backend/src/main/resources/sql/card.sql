7ff40aba1998f871093215917c1ad3677d6eb0eb
jihxn-kim <lizars4024@gmail.com>
2025-01-16 09:46:40 +0900

    Fix: 개인정보 삭제

--- app-backend/src/main/resources/sql/card.sql
+++ app-backend/src/main/resources/sql/card.sql
@@ -23,9 +23,5 @@ commit;
 
 drop table user_card;
 
--- d1oQotAZQHL8yDe7YXUFeG
 select * from user_card;
 select * from card;
-
-INSERT INTO user_card (USER_CARD_ID, USER_CARD_COMPANY_ID, USER_CARD_COMPANY_PWD, CREATED_AT, EXPIRY_DATE, UPDATED_AT, USER_CARD_NUM, USER_NAME, CARD_ID, USER_ID)
-VALUES (user_card_seq.nextval, 'wlgns4024', 'zhfmzm0908!', CURRENT_TIMESTAMP, "0627", CURRENT_TIMESTAMP, "4890160181422418", "현대신용카드", 7, 77);
