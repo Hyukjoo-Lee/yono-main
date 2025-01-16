3546893b2fb47e7ea41c61a3ed6b0f6024a75cd7
didieun <y-y555@nate.com>
2025-01-16 10:25:51 +0900

    update: card, user_card.sql 파일 수정

--- app-backend/src/main/resources/sql/user_card.sql
+++ app-backend/src/main/resources/sql/user_card.sql
@@ -3,4 +3,4 @@ drop table user_card;
 commit;
 
 INSERT INTO user_card (USER_CARD_ID, USER_CARD_COMPANY_ID, USER_CARD_COMPANY_PWD, CREATED_AT, EXPIRY_DATE, PRIMARY_CARD, UPDATED_AT, USER_CARD_NUM, USER_NAME, CARD_ID, USER_NUM)
-VALUES (user_card_seq.nextval, '카드사 아이디', '카드사 비밀번호호',  CURRENT_TIMESTAMP, '유효기간간', 0,  CURRENT_TIMESTAMP, '카드번호', '현대신용카드', 7, 77);
+VALUES (user_card_seq.nextval, 'playwithme31', 'test!!8520',  CURRENT_TIMESTAMP, '유효기간', 0,  CURRENT_TIMESTAMP, '4481258123549143', '하나체크크카드', 6, 1);
