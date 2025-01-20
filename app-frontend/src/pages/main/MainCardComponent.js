1da2730a0f807c3d648e582e19a45fdc107a494c
김채림 <rim1080@icloud.com>
2025-01-20 22:14:10 +0900

    update:main페이지 카드 정보 없을때 경고 문구 삽입했지만 실패

--- app-frontend/src/pages/main/MainCardComponent.js
+++ app-frontend/src/pages/main/MainCardComponent.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import CardImg from '../../assets/images/main_card.svg';
 
 const StyledCardContainer = styled.div`
   height: 285px;
@@ -43,36 +42,31 @@ const StyledCardInfo3 = styled.p`
 `;
 
 const MainCardComponent = () => {
-  const mainCardData = [
-    {
-      title: `현대카드 ZERO Edition3(할인형)`,
-      subtitle: `국내외 가맹점 0.8% 할인`,
-      info1: `스타벅스 50% 할인(월 1회)`,
-      info2: `대중교통 10% 할인(청구할인)`,
-      info3: `영화 쿠폰 제공(연 12회)`,
-      imgSrc: CardImg,
-    },
-  ];
+  const mainCardData = [];
   return (
     <StyledCardContainer>
-      {mainCardData.map((item) => (
-        <StyledCard1>
-          <img src={item.imgSrc} alt="메인카드" />
+      {mainCardData.length === 0 ? (
+        <p>등록된 카드가 없습니다.</p>
+      ) : (
+        mainCardData.map((item, index) => (
+          <StyledCard1 key={index}>
+            <img src={item.imgSrc} alt="메인카드" />
 
-          <StyledCardInfo>
-            <StyledCardInfo1>{item.title}</StyledCardInfo1>
-            <StyledCardInfo2>{item.subtitle}</StyledCardInfo2>
-            <StyledCardInfo3>
-              {item.info1}
-              <br />
-              {item.info2}
-              <br />
-              {item.info3}
-              <br />
-            </StyledCardInfo3>
-          </StyledCardInfo>
-        </StyledCard1>
-      ))}
+            <StyledCardInfo>
+              <StyledCardInfo1>{item.title}</StyledCardInfo1>
+              <StyledCardInfo2>{item.subtitle}</StyledCardInfo2>
+              <StyledCardInfo3>
+                {item.info1}
+                <br />
+                {item.info2}
+                <br />
+                {item.info3}
+                <br />
+              </StyledCardInfo3>
+            </StyledCardInfo>
+          </StyledCard1>
+        ))
+      )}
     </StyledCardContainer>
   );
 };
