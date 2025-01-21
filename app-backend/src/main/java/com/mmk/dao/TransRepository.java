3ca2fa9684157764958cd1de6fa490b8f7a2eb04
Hyukjoo-Lee <hjjaydon.lee@gmail.com>
2025-01-21 18:54:24 +0900

    Remove: 필요 없는 파일, 코드 제거

--- app-backend/src/main/java/com/mmk/dao/TransRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mmk.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mmk.entity.CardEntity;
-
-public interface TransRepository extends JpaRepository<CardEntity, Integer> {
-
-}
--- app-frontend/src/pages/mycard/MyCard.js
+++ app-frontend/src/pages/mycard/MyCard.js
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import CommonTabs from '../../common/CommonTabs';
 import CardRegTab from './CardRegTab';
 import CardRecTab from './CardRecTab';
 import styled from 'styled-components';
 
 import CommonRoot from '../../common/CommonRoot';
-import { saveCodefCard } from '../../apis/cardApi';
 
 const RootIn = styled.div`
   width: ${(props) => props.theme.display.lg};
@@ -16,21 +15,6 @@ const RootIn = styled.div`
 export function MyCard() {
   const [selectedTab, setSelectedTab] = useState(0);
   const items = [{ text: '카드 등록' }, { text: '카드 추천' }];
-
-  // Codef 에서 실제 가지고 보유하고 있는 카드리스트 + 혜택 정보 저장
-  // organization, userNum
-  useEffect(() => {
-    const fetchCardListData = async () => {
-      try {
-        const response = await saveCodefCard();
-        console.log('DB 저장 완료: ' + response);
-      } catch (error) {
-        console.error('카드 정보를 불러오는 중 오류 발생:', error);
-      }
-    };
-
-    fetchCardListData();
-  }, []);
 
   const panels = [<CardRegTab />, <CardRecTab />];
 
