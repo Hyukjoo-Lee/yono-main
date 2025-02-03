39a671b246b31fb32dea80ba6c95582358f1df2d
didieun <y-y555@nate.com>
2025-02-03 21:08:25 +0900

    update: badgeEntity 제약조건 다시 수정

--- app-backend/src/main/java/com/mmk/entity/BadgeEntity.java
+++ app-backend/src/main/java/com/mmk/entity/BadgeEntity.java
@@ -35,28 +35,20 @@ public class BadgeEntity {
     private String badgeDate; // 202501 -> 날짜
 
     @NotNull
-    @Size(max = 10000)
-    @Column(name = "badge", nullable = false, length = 10000, columnDefinition = "int default 0")
-    private int badge; // 12 -> 뱃지 갯수
+    @Column(name = "badge", nullable = false)
+    private int badge = 0; // 12 -> 뱃지 갯수
 
     @NotNull
-    @Size(max = 10000)
-    @Column(name = "ranking", nullable = false, length = 10000)
+    @Column(name = "ranking", nullable = false)
     private int ranking; // 랭킹 순위
-    
-    @NotNull
-    @Size(max = 5000)
-    @Column(name = "current_month_amount", nullable = false, length = 50000, columnDefinition = "int default 0")
-    private int currentMonthAmount; // 지난 달의 총 사용 금액
 
     @NotNull
-    @Size(max = 50000)
-    @Column(name = "previous_month_amount", nullable = false, length = 50000, columnDefinition = "int default 0")
-    private int previousMonthAmount; // 저저번 달 총 사용 금액
+    @Column(name = "current_month_amount", nullable = false)
+    private int currentMonthAmount = 0; // 지난 달의 총 사용 금액
 
-    // @ManyToOne
-    // @JoinColumn(name = "card_History", nullable = false)
-    // private CardHistoryEntity cardHistoryEntity;
+    @NotNull
+    @Column(name = "previous_month_amount", nullable = false)
+    private int previousMonthAmount = 0; // 저저번 달 총 사용 금액
 
     @ManyToOne
     @JoinColumn(name = "user_num", referencedColumnName = "user_num", nullable = false, foreignKey = @ForeignKey(name = "fk_badge_user_num"))
