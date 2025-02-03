0338d57422529b6254892f939e57692dcdc2e96b
didieun <y-y555@nate.com>
2025-02-03 20:01:43 +0900

    update: badgeEntity -> 테이블 타입 수정

--- app-backend/src/main/java/com/mmk/entity/BadgeEntity.java
+++ app-backend/src/main/java/com/mmk/entity/BadgeEntity.java
@@ -2,6 +2,7 @@ package com.mmk.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -9,6 +10,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -26,19 +29,29 @@ public class BadgeEntity {
     @Column(name = "badge_num")
     private int badgeNum;
 
-    @Column(name = "badge_date")
+    @NotNull
+    @Size(max = 6)
+    @Column(name = "badge_date", nullable = false, length = 6)
     private String badgeDate; // 202501 -> 날짜
 
-    @Column(name = "badge")
+    @NotNull
+    @Size(max = 10000)
+    @Column(name = "badge", nullable = false, length = 10000, columnDefinition = "int default 0")
     private int badge; // 12 -> 뱃지 갯수
 
-    @Column(name = "ranking")
+    @NotNull
+    @Size(max = 10000)
+    @Column(name = "ranking", nullable = false, length = 10000)
     private int ranking; // 랭킹 순위
     
-    @Column(name = "current_month_amount")
+    @NotNull
+    @Size(max = 5000)
+    @Column(name = "current_month_amount", nullable = false, length = 50000, columnDefinition = "int default 0")
     private int currentMonthAmount; // 지난 달의 총 사용 금액
 
-    @Column(name = "previous_month_amount")
+    @NotNull
+    @Size(max = 50000)
+    @Column(name = "previous_month_amount", nullable = false, length = 50000, columnDefinition = "int default 0")
     private int previousMonthAmount; // 저저번 달 총 사용 금액
 
     // @ManyToOne
@@ -46,6 +59,6 @@ public class BadgeEntity {
     // private CardHistoryEntity cardHistoryEntity;
 
     @ManyToOne
-    @JoinColumn(name = "user_num", nullable = false)
+    @JoinColumn(name = "user_num", referencedColumnName = "user_num", nullable = false, foreignKey = @ForeignKey(name = "fk_badge_user_num"))
     private UserEntity userEntity;
 }
