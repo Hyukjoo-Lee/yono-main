f47f6fb9dcd4cda2334d204012848259309daa0d
김채림 <rim1080@icloud.com>
2025-02-07 14:56:06 +0900

    update: sql 다시변경

--- app-backend/src/main/resources/sql/badge.sql
+++ app-backend/src/main/resources/sql/badge.sql
@@ -1,9 +1,5 @@
-create sequence badge_seq
-start with 1
-increment by 1
-nocache;
-
 select * from badge;
+delete from badge where user_num = 45;
 
 drop table badge;
 
@@ -11,11 +7,3 @@ DROP SEQUENCE badge_seq;
 TRUNCATE table badge;
 
 commit;
-
-CREATE SEQUENCE badge_seq
-START WITH 1
-INCREMENT BY 1
-NOCACHE
-NOCYCLE;
-
-
